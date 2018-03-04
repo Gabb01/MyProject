790ccef78e85d78adf540fb503cf251c04aeb986
Gabriel Rata <gabb04is@gmail.com>
2018-03-04 13:04:23 +0200

    First commit for REST App

---
+++ Licenta2018/src/test/User.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement(name = "user")
+public class User implements Serializable {
+
+   private static final long serialVersionUID = 1L;
+   private int id;
+   private String nume;
+   private String oras;
+
+   public User(){}
+
+   public User(int id, String nume, String oras){
+      this.id = id;
+      this.nume = nume;
+      this.oras = oras;
+   }
+
+   public int getId() {
+      return id;
+   }
+   @XmlElement
+   public void setId(int id) {
+      this.id = id;
+   }
+   public String getName() {
+      return nume;
+   }
+   @XmlElement
+      public void setName(String nume) {
+      this.nume = nume;
+   }
+   public String getCity() {
+      return oras;
+   }
+   @XmlElement
+   public void setCity(String oras) {
+      this.oras = oras;
+   }	
+
+   @Override
+   public boolean equals(Object object){
+      if(object == null){
+         return false;
+      }else if(!(object instanceof User)){
+         return false;
+      }else {
+         User user = (User)object;
+         if(id == user.getId()
+            && nume.equals(user.getName())
+            && oras.equals(user.getCity())
+         ){
+            return true;
+         }			
+      }
+      return false;
+   }	
+}
